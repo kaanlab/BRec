4b4d0c3ce0754cf53f9266d4183a4886a875a850
kaanlab <kaan@live.ru>
2021-02-04 23:15:21 +0300

    Initial commit

---
+++ BRec.Web/Controllers/Upload.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BRec.Web.Controllers
+{
+    public class Upload : Controller
+    {
+        IWebHostEnvironment _hostingEnvironment;
+
+        public Upload(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+
+        }
+
+        [Route("api/[controller]/Save")]
+        [HttpPost]
+        public async Task<IActionResult> Save(IFormFile file)
+        {
+            //if (file.ContentType != "audio/wav")
+            //{
+            //    return BadRequest("Wrong file type");
+            //}
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");//uploads where you want to save data inside wwwroot
+
+            //var filePath = Path.Combine(uploads, Path.GetRandomFileName() + ".wav");
+            var filePath = Path.Combine(uploads, file.FileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return Ok("File uploaded successfully");
+        }
+    }
+}
---
+++ BRec.Web/wwwroot/js/recorder.js
@@ -0,0 +1,64 @@
+﻿// New instance
+const recorder = new MicRecorder({
+    bitRate: 128
+});
+
+window.MyRecorder = {
+
+    // Start recording. Browser will request permission to use your microphone.
+    start: function () {
+        recorder.start().then(() => {
+            // something else
+        }).catch((e) => {
+            console.error(e);
+        });
+    },
+
+    // Once you are done singing your best song, stop and get the mp3.
+    stop: function (param) {
+        recorder.stop()
+            .getMp3().then(([buffer, blob]) => {
+                // do what ever you want with buffer and blob
+                // Example: Create a mp3 file and play
+                const filename = param + '.mp3';
+                const file = new File(buffer, filename, {
+                    type: blob.type,
+                    lastModified: Date.now()
+                });
+
+                const a = document.createElement("a");
+                document.body.appendChild(a);
+                a.style = "display: none";
+                const li = document.createElement('span');
+                const player = new Audio(URL.createObjectURL(file));
+                player.controls = true;
+                li.appendChild(player);
+                document.querySelector('#playlist').appendChild(li);
+                a.href = URL.createObjectURL(file);
+                a.download = file.name;
+                a.click();
+                const fd = new FormData();
+                fd.append("file", file, file.name);
+                const xhr = new XMLHttpRequest();
+                xhr.addEventListener("load", transferComplete);
+                xhr.addEventListener("error", transferFailed)
+                xhr.addEventListener("abort", transferFailed)
+                xhr.open("POST", "api/Upload/Save/", true);
+                xhr.send(fd);
+
+            }).catch((e) => {
+                alert('We could not retrieve your message');
+                console.log(e);
+            });
+    }
+};
+
+function transferComplete(evt) {
+    console.log("The transfer is complete.");
+    //GLOBAL.DotNetReference.invokeMethodAsync('Recognize', filename);
+}
+function transferFailed(evt) {
+    console.log("An error occurred while transferring the file.");
+    console.log(evt.responseText);
+    console.log(evt.status);
+}
